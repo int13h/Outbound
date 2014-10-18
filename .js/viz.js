fdba604052608408d77645635ea64b2209dd5a1c
int13h <paul.halliday@gmail.com>
2014-10-18 11:53:47 -0300

    slider for tables

---
+++ .js/viz.js
@@ -0,0 +1,80 @@
+// Slider for data tables. Copied most of it from the d3 brush examples.
+function mkSlider(callerID,low,high) {
+  var margin = {top: 3, right: 15, bottom: 3, left: 5},
+      width = 100,
+      height = 10;
+
+  // Only produce a slider if we need to
+  if (high <= 1) { 
+    d3.select("#" + callerID).text(""); 
+    return;
+  }
+
+  if (high > 1000) high = 1000;
+
+  var x = d3.scale.linear()
+      .domain([1, high]) 
+      .range([1, width])
+      .clamp(true);
+
+  var brush = d3.svg.brush()
+      .x(x)  
+      .extent([0, 0])
+      .on("brush", brushed);
+
+  d3.select("#" + callerID)
+       .text("");
+
+  var svg = d3.select("#" + callerID).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("g")
+    .attr("class", "x slideraxis")
+    .attr("transform", "translate(0," + height / 2 + ")")
+    .call(d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .tickFormat(function(d) { return d; })
+      .tickSize(0)
+      .tickPadding(7))
+    .select(".domain")
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "sliderhalo");
+
+  var slider = svg.append("g")
+      .attr("class", "slider")
+      .call(brush);
+
+  slider.selectAll(".extent,.resize")
+    .remove();
+
+  slider.select(".background")
+    .attr("height", height);
+
+  var handle = slider.append("circle")
+    .attr("class", "sliderhandle")
+    .attr("transform", "translate(0," + height / 2 + ")")
+    .attr("r", 5);
+
+  // Position the brush at our current selection
+  slider
+    .call(brush.event)
+    .transition()
+    .duration(0)
+    .call(brush.extent([low, low]))
+    .call(brush.event);
+
+  function brushed() {
+    var value = brush.extent()[0];
+    if (d3.event.sourceEvent) {
+      value = x.invert(d3.mouse(this)[0]);
+      brush.extent([value, value]);
+    }
+
+    handle.attr("cx", x(value));
+    d3.select("#" + callerID + "_lbl").html(value.toFixed(0));
+  }
+}
