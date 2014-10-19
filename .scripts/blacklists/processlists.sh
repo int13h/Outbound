ba46875c84e1e67c3bb75fd11fff6f3359afddb6
int13h <paul.halliday@gmail.com>
2014-10-19 10:29:43 -0300

    added support for IP lists

--- .scripts/blacklists/processlists.sh
+++ .scripts/blacklists/processlists.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# These can be domains(0) or IP lists(1).
+# If domains we need to add the trailing dot.
+# This should be called via cron daily.
 
 lists="\
-http://mirror1.malwaredomains.com/files/justdomains \
-https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist
+http://mirror1.malwaredomains.com/files/justdomains|0 \
+https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist|0 \
+http://rules.emergingthreats.net/blockrules/compromised-ips.txt|1
 "
 
 workdir="$PWD"
@@ -16,13 +20,21 @@ function cleanup {
 cleanup
 
 for bl in $lists; do
-  wget -O tmp --no-check-certificate $bl 
-  cat $workdir/tmp >> temp.txt
+  list=`echo $bl | awk -F "|" '{print $1}'`  
+  type=`echo $bl | awk -F "|" '{print $2}'`
+  wget -O tmp --no-check-certificate $list 
+
+  if [ $type -ne 0 ]; then
+    cat $workdir/tmp >> temp.txt
+  else
+    cat $workdir/tmp | sed 's/$/./' >> temp.txt
+  fi
+
   rm -f $workdir/tmp
 done;
 
 # Remove duplicates and normalize text
-cat $workdir/temp.txt | grep -v ^# | grep -v "^$" | sed 's/  / /g' | tr '[A-Z]' '[a-z]' | sed 's/$/./'| sort | uniq > $workdir/insert.txt
+cat $workdir/temp.txt | grep -v ^# | grep -v "^$" | sed 's/  / /g' | tr '[A-Z]' '[a-z]' | sort | uniq > $workdir/insert.txt
 rm -f $workdir/temp.txt
 
 # Cleanup (this will be more robust later. PoC for now. Also won't be in sh either :/
